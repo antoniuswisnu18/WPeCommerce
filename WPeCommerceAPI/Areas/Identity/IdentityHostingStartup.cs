026f43a86706fd04fe33575362c06bb0ac2ee1c1
Wisnu Prasojo <wisnuprasojo14@gmail.com>
2024-01-25 20:27:09 +0700

    Revert back before implementing core Identity to API

---
+++ WPeCommerceAPI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WPeCommerceAPI.DataLayer;
+
+[assembly: HostingStartup(typeof(WPeCommerceAPI.Areas.Identity.IdentityHostingStartup))]
+namespace WPeCommerceAPI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
